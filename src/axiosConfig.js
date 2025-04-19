cd3454cc1d4a84443ef82872ea05ba72b1652bc4
Ashenafi <ashenafi5050@icloud.com>
2025-04-19 15:21:42 -0400

    first commit

---
+++ src/axiosConfig.js
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+const Instance = axios.create({
+  // baseURL: "https://evangadi-forum-backend-i25l.onrender.com/api",
+  baseURL: "http://localhost:5500/api",
+});
+
+export default Instance;
